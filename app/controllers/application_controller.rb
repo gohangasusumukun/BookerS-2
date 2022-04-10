efd46d49d7db1ceed402352d8ce756f6c0d33b96
gohangasusumukun <isecha.ocha.cha.sket@gmail.com>
2022-04-10 19:05:25 +0000

    [Update] ユーザ登録認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
   
-  
-
+  # device利用の機能が使用される場合、:configure_permitted~が実行される
+  before_action :configure_permitted_parameters,if: :devise_controller?
+  # 他のコントローラからも参照できる
   protected
-
+  # signupの際に、nameのデータ操作が許可される
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
   end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,11 @@ class UsersController < ApplicationController
      @user = User.find(params[:id])
      @post_images = @user.post_images.page(params[:page]).reverse_order
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
@@ -18,4 +18,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
+
 end
