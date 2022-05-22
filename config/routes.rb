4fc6c848e14845c7814de1bf29c5435c82c6d29b
gohangasusumukun <isecha.ocha.cha.sket@gmail.com>
2022-05-22 06:13:04 +0000

    [add]

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
  devise_for :users
   root 'homes#top'
-  get "about" => "books#about"
+  get "home/about" => "homes#about"
   resources :books,only:[:new,:create,:index,:show,:destroy,:edit,:update]
   resources :users,only:[:new,:create,:index,:show,:edit,:update]
 end
